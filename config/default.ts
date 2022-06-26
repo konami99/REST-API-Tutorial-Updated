04055b2b9cf56b07e871e4156be7bc39d86784a0
Richard Chou <konami99@hotmail.com>
2022-06-26 11:57:50 +1000

    updated connection string

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
   port: 1337,
-  dbUri: "mongodb://mongodatabase:27017/rest-api-tutorial",
+  dbUri: "mongodb://konami99:7^b!Bqzo4Xer@mongodatabase:27017/rest-api-tutorial",
   saltWorkFactor: 10,
   accessTokenTtl: "15m",
   refreshTokenTtl: "1y",
